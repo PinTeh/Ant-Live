41b6a540c4edbe9f267fdc5e0f23fccaf0a0c85a
pinteh <794409767@qq.com>
2022-05-08 10:56:21 +0800

    chore: 移除阿里云服务监控相关文件

--- src/main/java/cn/imhtb/antlive/controller/CategoryController.java
+++ src/main/java/cn/imhtb/antlive/controller/CategoryController.java
@@ -6,7 +6,6 @@ import cn.imhtb.antlive.entity.database.Category;
 import cn.imhtb.antlive.service.ICategoryService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,11 +25,11 @@ public class CategoryController {
     }
 
     @GetMapping("/list")
-    @Cacheable("category")
     public ApiResponse list(@RequestParam(required = false, defaultValue = "10") Integer limit
             , @RequestParam(required = false, defaultValue = "1") Integer page){
         QueryWrapper<Category> wrapper = new QueryWrapper<Category>().eq("disabled", Constants.DisabledStatus.YES.getCode()).orderByDesc("sort");
         Page<Category> categoryPage = categoryService.page(new Page<>(page, limit), wrapper);
         return ApiResponse.ofSuccess(categoryPage);
     }
+
 }

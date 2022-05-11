79a4a492d7b30786b58dbc310971a6e7a19a1622
pinteh <794409767@qq.com>
2022-05-11 22:41:00 +0800

    refactor: 重构优化分类接口

--- src/main/java/cn/imhtb/antlive/controller/CategoryController.java
+++ src/main/java/cn/imhtb/antlive/controller/CategoryController.java
@@ -1,13 +1,9 @@
 package cn.imhtb.antlive.controller;
 
 import cn.imhtb.antlive.common.ApiResponse;
-import cn.imhtb.antlive.common.Constants;
-import cn.imhtb.antlive.entity.database.Category;
 import cn.imhtb.antlive.service.ICategoryService;
 import cn.imhtb.antlive.vo.PageWrapperResp;
 import cn.imhtb.antlive.vo.response.CategoryResp;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,14 +20,6 @@ public class CategoryController {
 
     @Resource
     private ICategoryService categoryService;
-
-    @GetMapping("/list")
-    public ApiResponse list(@RequestParam(required = false, defaultValue = "10") Integer limit,
-                            @RequestParam(required = false, defaultValue = "1") Integer page){
-        QueryWrapper<Category> wrapper = new QueryWrapper<Category>().eq("disabled", Constants.DisabledStatus.YES.getCode()).orderByDesc("sort");
-        Page<Category> categoryPage = categoryService.page(new Page<>(page, limit), wrapper);
-        return ApiResponse.ofSuccess(categoryPage);
-    }
 
     @GetMapping("/query")
     public ApiResponse queryCategoryPage(@RequestParam(required = false, defaultValue = "10") Integer limit,
